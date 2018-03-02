b2494c80867cc93254f9440436c67cccb6324069
tbritt10 <35700873+tbritt10@users.noreply.github.com>
2018-03-02 12:17:13 -0600

    Added Gregorian import for punch, left TODO notes

--- src/tasproject/Punch.java
+++ src/tasproject/Punch.java
@@ -6,16 +6,22 @@ package tasproject;
  * Mar 2, 2018
  */
 
+import java.util.GregorianCalendar;
+
 public class Punch {
 
-	int terminalid;
-	int punchtypeid;
-	Badge badge = new Badge(); //Not for sure about this just making the placeholder.
-	// ^ This is the minimal information needed to represent the punch.
-	// The other properties of this object (such as the ID and the adjusted timestamp) should be initialized to zero or to null, according to their type.
-	public Punch(Badge badge, int terminalid, int punchtypeid) {
-  		this.terminalid = terminalid;
-    	this.punchtypeid = punchtypeid;
-    	this.badge = badge; // Not sure if we will pass an int for the badge number here or what; just a placeholder
-  	}
+    int terminalid = 0;
+    int punchtypeid;
+    String id;
+    String description;
+    
+    Badge badge = new Badge(id, description); 
+    // ^ This is the minimal information needed to represent the punch.
+    // The other properties of this object (such as the ID and the adjusted timestamp) should be initialized to zero or to null, according to their type.
+    public Punch(Badge badge, int terminalid, int punchtypeid) {
+        //TODO: Add gregoriancalendar objects for date and time of Punches. Add to args of punch constructor
+        this.terminalid = terminalid;
+        this.punchtypeid = punchtypeid;
+        this.badge = badge; 
+    }
 }
