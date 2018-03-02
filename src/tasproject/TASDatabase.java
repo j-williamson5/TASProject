995f455504fee17006f737a6ae747f9d2eb68640
Josh <Josh@10.80.31.70>
2018-03-02 12:39:58 -0600

    Database Constructor Made

--- src/tasproject/TASDatabase.java
+++ src/tasproject/TASDatabase.java
@@ -3,10 +3,38 @@
  */
 package tasproject;
 import java.sql.*;
+import java.lang.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Josh and Matthew
  */
 public class TASDatabase {
+    //Instance fields
+     String url;
+     String username;
+     String password;
+     Connection conn;
+     Statement stmt;
+     //Constructor
+    public TASDatabase() throws SQLException, InstantiationException, IllegalAccessException{
+        this.url = "jdbc:mysql://localhost/tas";
+        this.username = "tasuser";
+        this.password = "teamone";
+        establishConnections();
+    }
+    //Methods
+    private void establishConnections() throws SQLException, InstantiationException, IllegalAccessException{
+         try {
+             Class.forName("com.mysql.jdbc.Driver").newInstance();
+             DriverManager.getConnection(url, username,password);
+             this.stmt = conn.createStatement();
+         } catch (ClassNotFoundException ex) {
+             Logger.getLogger(TASDatabase.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+    }
     
+
 }
