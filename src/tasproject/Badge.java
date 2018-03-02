0eb6f18c3b2329ffc2488c6d163fb86e7b42490f
Dallen <Dallen@10.80.30.185>
2018-03-02 11:53:19 -0500

    Finished Badge Class

---
+++ src/tasproject/Badge.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasproject;
+
+/**
+ *
+ * @author Dallen
+ */
+public class Badge {
+    private String id;
+    private String description;
+    public Badge(){
+        this.id = id;
+        this.description = description;
+    }
+    
+    public String getID(){
+		return this.id;
+	}
+
+
+	public String getDescription(){
+		return this.description;
+	}
+
+	
+	//Formatting the output to look like the following: #D2CC71D4 (Lawson, Matthew J)
+
+	public String toString(){
+		return "#" + this.getID() + " " + "(" + this.getDescription() + ")";
+		
+	}
+    
+}
