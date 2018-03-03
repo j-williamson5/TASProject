83afec3c20bede0365e7e3f8381f56b401cab078
Josh <Josh@10.80.31.70>
2018-03-03 13:38:59 -0600

    Made Setters and Getters

--- src/tasproject/Punch.java
+++ src/tasproject/Punch.java
@@ -44,13 +44,128 @@ public class Punch {
         return calendar;
     }
     
+    /*
     public String toString(Punch punch) {
         return (badge.getID() + " CLOCKED IN: "); //Not sure how to format the data as of yet, need to follow the example below.
     }
+    */
     
+    //Placeholder for feature 3
+    public String printOriginalTimestamp(){
+        return null;//Change null after feature 3
+    }
+
+    //Setters and Getters - Josh
+    public int getTerminalid() {
+        return terminalid;
+    }
+
+    public void setTerminalid(int terminalid) {
+        this.terminalid = terminalid;
+    }
+
+    public int getPunchtypeid() {
+        return punchtypeid;
+    }
+
+    public void setPunchtypeid(int punchtypeid) {
+        this.punchtypeid = punchtypeid;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+
+    public void setBadge(Badge badge) {
+        this.badge = badge;
+    }
+
+    public GregorianCalendar getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(GregorianCalendar timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    
+    /*What is this??? -Josh
     public Punch getPunch(int terminalid) {
         return null; 
         //Below is an example of exactly what the output from this function should look like. To string method for the first part.
         //assertEquals("#D2C39273 CLOCKED IN: WED 09/06/2017 07:00:07", p1.printOriginalTimestamp());
     }
+    */
 }
