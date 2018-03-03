9379a43a5fc0ce056f302d3d9499cbc83f15020c
Matthew Minor <mminor1@stu.jsu.edu>
2018-03-03 13:30:29 -0600

    redid some things in database

--- src/tasproject/Badge.java
+++ src/tasproject/Badge.java
@@ -29,11 +29,18 @@ public class Badge {
         return this.id;
     }
 
-
     public String getDescription(){
         return this.description;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
     //Formatting the output to look like the following: #D2CC71D4 (Lawson, Matthew J)
     public String toString(){
         return "#" + this.getID() + " " + "(" + this.getDescription() + ")";
