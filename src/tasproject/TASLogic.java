910b2ec96bed59ffc03feba345cd69c19be6eae6
Dallen <Dallen@10.80.30.15>
2018-03-14 11:33:26 -0500

    Creating time objects to calculate total minutes

--- src/tasproject/TASLogic.java
+++ src/tasproject/TASLogic.java
@@ -5,10 +5,43 @@
  */
 package tasproject;
 
+import java.util.ArrayList;
+
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author madel
  */
 public class TASLogic {
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+        int totalMinutes;
+        int totalInMillis;
+        
+        // Creating time objects for each aspect of the shift 
+        
+        GregorianCalendar startTime = new GregorianCalendar();
+        startTime.setTimeInMillis(shift.getStartTime().getTime());
+        
+        GregorianCalendar stopTime = new GregorianCalendar();
+        stopTime.setTimeInMillis(shift.getStopTime().getTime());
+        
+        GregorianCalendar lunchStart = new GregorianCalendar();
+        lunchStart.setTimeInMillis(shift.getLunchStart().getTime());
+        
+        GregorianCalendar lunchStop = new GregorianCalendar();
+        lunchStop.setTimeInMillis(shift.getLunchStop().getTime());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return totalMinutes;
+    }
     
 }
