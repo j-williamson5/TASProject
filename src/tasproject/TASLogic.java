a4f8e11724da74cede2e3f2c441fc948a3a6f7fb
Dallen <Dallen@10.80.30.15>
2018-03-16 11:51:53 -0500

    More completed on feature 4

--- src/tasproject/TASLogic.java
+++ src/tasproject/TASLogic.java
@@ -20,6 +20,7 @@ public class TASLogic {
         int totalMinutes = 0;
         int totalInMillis = 0;
         boolean insidePair;
+        Punch p = new Punch();
         
         //ALGORITHM - Looping through the punch list and finding a "CLOCK IN" punch and a corresponding "CLOCK OUT" punch. Once we find those two we are
         //inside of a pair. Once inside a pair we calculate the minutes. After that we continue loop from the last "CLOCK OUT" punch. 
@@ -29,16 +30,20 @@ public class TASLogic {
             //Getting a "CLOCK IN punch"
             if(dailyPunchList.get(i).getPunchtypeid() == 1){
                 //boolean flag to flip once we find a "CLOCK OUT" punch.
+                GregorianCalendar clockIn = dailyPunchList.get(i).getAdjustedTimeStamp();
+                long inInMillis = clockIn.getTimeInMillis();
                 insidePair = false;
                
                 //Searching for a corresponding "CLOCK OUT" punch to pair the "CLOCK IN" with.
-                for(int j =outFoundAt; j < dailyPunchList.size();++j){
+                for(int j = outFoundAt; j < dailyPunchList.size();++j){
                     if(dailyPunchList.get(j).getPunchtypeid() == 0){
+                        GregorianCalendar clockOut = dailyPunchList.get(j).getAdjustedTimeStamp();
+                        long outInMillis = clockOut.getTimeInMillis();
                         insidePair = true;
                         outFoundAt = j;
-                        // We are inside a "CLOCK IN" and "CLOCK OUT" pair. Now we need to calculate minutes in between the two.
+                        // We are inside a "CLOCK IN" and "CLOCK OUT" pair. Now we need to calculate minutes in between the two and convert to minutes.
                         if(insidePair){
-                            
+                           int workedTimeInMins = p.millisToMinutes(outInMillis - inInMillis);
                         }
                     }
                     
@@ -46,7 +51,7 @@ public class TASLogic {
                 }
             }
         }
-      
+      return totalMinutes; 
         
 
     }
